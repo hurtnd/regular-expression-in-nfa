898a7bbc0fcb80417d1d3db7b72de7691b770c62
hurtnd <57873165+hurtnd@users.noreply.github.com>
2023-05-14 16:34:16 +0300

    Made the code more readable

--- RegExGUI.py
+++ RegExGUI.py
@@ -34,34 +34,43 @@ class RegExGUI:
 
         self.window.geometry(f"{width}x{height}+{x}+{y}")
 
-        self.regex_entry = ctk.CTkEntry(self.window, placeholder_text="Enter a regular expression",
-                                        width=250, height=30)
+        self.regex_entry = ctk.CTkEntry(
+            self.window, placeholder_text="Enter a regular expression",
+            width=250, height=30)
         self.regex_entry.grid(row=0, column=0, padx=25, pady=20, sticky="ew")
-        self.word_entry = ctk.CTkEntry(self.window, placeholder_text="Enter the word",
-                                       width=250, height=30)
+        self.word_entry = ctk.CTkEntry(
+            self.window, placeholder_text="Enter the word",
+            width=250, height=30)
         self.word_entry.grid(row=1, column=0, padx=25, pady=0, sticky="ew")
 
-        self.check_button = ctk.CTkButton(self.window, text="Check", command=self.check_regex)
+        self.check_button = ctk.CTkButton(
+            self.window, text="Check", command=self.check_regex)
         self.check_button.grid(row=2, column=0, padx=25, pady=20)
 
         def bind_enter_button(event):
             return self.check_regex()
-        
+
         self.window.bind('<Return>', bind_enter_button)
 
-        self.startup = ctk.CTkImage(light_image=Image.open(resource_path("assets/waving_hand.png")), 
-                                    size=(STICKER_SIZE, STICKER_SIZE))
-        self.thumbs_up = ctk.CTkImage(light_image=Image.open(resource_path("assets/thumbs_up.png")), 
-                                      size=(STICKER_SIZE, STICKER_SIZE))
-        self.thumbs_down = ctk.CTkImage(light_image=Image.open(resource_path("assets/thumbs_down.png")), 
-                                        size=(STICKER_SIZE, STICKER_SIZE))
-        self.pop_up_sticker = ctk.CTkLabel(master=self.window, image=self.startup, text="")
+        self.startup = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/waving_hand.png")),
+            size=(STICKER_SIZE, STICKER_SIZE))
+        self.thumbs_up = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/thumbs_up.png")),
+            size=(STICKER_SIZE, STICKER_SIZE))
+        self.thumbs_down = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/thumbs_down.png")),
+            size=(STICKER_SIZE, STICKER_SIZE))
+        self.pop_up_sticker = ctk.CTkLabel(
+            master=self.window, image=self.startup, text="")
         self.pop_up_sticker.grid(row=3, column=0, padx=0, pady=15)
 
-        self.result_label = ctk.CTkLabel(self.window, text="", text_color="#A9A9A9")
+        self.result_label = ctk.CTkLabel(
+            self.window, text="", text_color="#A9A9A9")
         self.result_label.grid()
 
-        self.company_label = ctk.CTkLabel(self.window, text="© 2023 Mesa Research", text_color="#696969")
+        self.company_label = ctk.CTkLabel(
+            self.window, text="© 2023 Mesa Research", text_color="#696969")
         self.company_label.grid()
 
         self.window.mainloop()
@@ -75,11 +84,13 @@ class RegExGUI:
             return
         if word == "":
             self.pop_up_sticker.configure(image=self.thumbs_down)
-            self.result_label.configure(text=f"Enter the word")
+            self.result_label.configure(text="Enter the word")
         else:
             if match(regex, word):
                 self.pop_up_sticker.configure(image=self.thumbs_up)
-                self.result_label.configure(text=f"The word belongs to a regular expression")
+                self.result_label.configure(
+                    text="The word belongs to a regular expression")
             else:
                 self.pop_up_sticker.configure(image=self.thumbs_down)
-                self.result_label.configure(text=f"The word doesn't belong to a regular expression")
+                self.result_label.configure(
+                    text="The word doesn't belong to a regular expression")
